0bf83dc8b3ade81e371e76b49778e936afa18e50
imran6099 <nagatoyahiko856@gmail.com>
2022-11-01 10:51:52 +0300

    updates

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -6,6 +6,8 @@ const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_API || 'https://local-asset-management.herokuapp.com/v1/',
 });
 
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = true;
+
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
